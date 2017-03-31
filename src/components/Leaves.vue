120a22c3206289bbed1fafd716e8ab57acac4bd5
bram <bramvereertbrugghen@outlook.com>
2017-03-31 09:25:37 +0200

    cleaned up the styling for each element based on the leavestatus

--- src/components/Leaves.vue
+++ src/components/Leaves.vue
@@ -5,41 +5,41 @@ div(class='calendar')
       h3 My Leaves
       p Here, have an overview of all your leaves. Whatever
         div
-          #accordion(role='tablist', aria-multiselectable='true')
-
-            //- Ribbon colour based on leave.status -> Red, Green, Blue
-            .card(v-for='(leave, i) in leaves' v-bind:class='[{"card-top-green" : leave.status == leaveStatuses[2]}, {"card-top-red" : leave.status == leaveStatuses[1]}, "card-top-blue" ]')
-
+          #accordion(v-for='(leave, i) in sortedLeaves' role='tablist', aria-multiselectable='true')
+            hr(v-if='leave.leavedate_set')
+            .card( v-bind:class='getRibbonStyleClass(leave)')
               div.card-header(role='tab', v-bind:id='"heading-" + i', data-toggle='collapse', data-parent='#accordion', aria-expanded='false', 'v-bind:aria-controls='"collapse-" + i', 'v-bind:href='"#collapse-" + i')
                 div.row
                   div.col-md-10
                     a
                       | {{ leave.description }}
                   div.col-md-2 
-
-                    //- Text colour based on leave.status -> Red, Green, Blue
-                    span.tag.float-md-right(v-bind:class=' [{"tag-success" : leave.status == leaveStatuses[2]}, {"tag-danger": leave.status == leaveStatuses[1]}, "tag-primary" ] ') {{ leave.status }}
+                    span.tag.float-md-right(v-bind:class='getTagStyleClass(leave)') 
+                      | {{ leave.status }}
                     
               div.collapse(role='tabpanel', v-bind:id='"collapse-" + i', v-bind:aria-labelledby='"heading-" + i')
                 div.card-block
                   div
                     ul.list-group
                       li.list-group-item(v-for='leave_date in leave.leavedate_set')
-                        | <strong>From:</strong> {{ leave_date.starts_at | moment('DD MMM YYYY hh:mm') }}<br>
-                        | <strong>To:</strong> {{ leave_date.ends_at | moment('DD MMM YYYY hh:mm') }}
+                        | <strong>From:</strong> {{ leave_date.starts_at | moment('DD MMM YYYY - HH:mm') }}<br>
+                        | <strong>To:</strong> {{ leave_date.ends_at | moment('DD MMM YYYY - HH:mm') }}
     div.col-md-6
       cmpHolidays
+
+
 </template>
 <script>
-import { mapState } from 'vuex'
-import * as types from '../store/mutation-types'
-import * as constant from '../store/constants'
-import store from '../store'
-import Holidays from './Holidays.vue'
+import { mapState } from 'vuex';
+import * as types from '../store/mutation-types';
+import * as constant from '../store/constants';
+import store from '../store';
+import Holidays from './Holidays.vue';
+import LeaveForm from './forms/LeaveForm.vue';
+import moment from 'moment';
 
 var data = {
   leaves: [],
-  leaveStatuses: constant.LEAVE_STATUSES,
 }
 
 export default {
@@ -53,25 +53,83 @@ export default {
     cmpHolidays: Holidays,
   },
 
-  computed: {
-    //Calculate the correct ribbon class for styling purposes
-    ribbonClass: function(leave) {
-      console.log(leave);
-      return 'card-top-blue';
-    }
-
-    //Calculate the correct tag class for styling purposes
-
-  },
-
   created: () => {
     store.dispatch(types.NINETOFIVER_API_REQUEST, {
       path: '/my_leaves/',
     }).then((response) => {
+
+      response.data.results.forEach(x => {
+        x.leavedate_set.forEach(x => {
+          x.starts_at = moment(x.starts_at, 'YYYY-MM-DD HH:mm:ss');
+          x.ends_at = moment(x.ends_at, 'YYYY-MM-DD HH:mm:ss');
+        });
+      });
+
       data.leaves = response.data.results
     }, () => {
       this.loading = false
     })
+  },
+
+  computed: { 
+
+    sortedLeaves: function() {
+      return this.leaves;
+      // .sort(function(a,b) {
+
+      //   var thing1 = a.leavedate_set.find(Math.min.apply(Math, function(o) {
+      //     return o.starts_at
+      //     }));
+      //   var thing2 = b.leavedate_set.find(Math.min.apply(Math, function(o) {
+      //       return o.starts_at
+      //     }));
+
+        
+      //   if(thing1 < thing2)
+      //     return -1;
+      //   else if(thing1 == thing2)
+      //     return 0;
+      //   else
+      //     return 1;
+
+      // });
+    },
+
+    nearestLeave: function() {
+      // var today = new Date();
+      // var leaveLength = this.leaves.length;
+
+      // for(var i = 0; i < leaveLength; i++) {
+
+      }
+
+    },
+  },
+
+  methods: {
+
+    getRibbonStyleClass: function(leave) {
+      //Rejected
+      if(leave.status === constant.LEAVE_STATUSES[1])
+        return 'card-top-red'; 
+      //Approved
+      else if(leave.status === constant.LEAVE_STATUSES[2])
+        return 'card-top-green';
+      //Default
+      return 'card-top-blue';                                 
+    },
+
+    getTagStyleClass: function(leave) {
+      //Rejected
+      if(leave.status === constant.LEAVE_STATUSES[1])
+        return 'tag-danger';
+      //Approved
+      else if(leave.status === constant.LEAVE_STATUSES[2])
+        return 'tag-success';
+      //Default
+      return 'tag-primary';  
+    }
+
   },
 }
 </script>
