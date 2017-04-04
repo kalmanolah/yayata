621f898050c28ab99693c0419764120f915925a9
bram <bramvereertbrugghen@outlook.com>
2017-04-04 15:43:45 +0200

    Start refactor constants to VueX's state principle

--- src/store/modules/ninetofiver.js
+++ src/store/modules/ninetofiver.js
@@ -5,12 +5,66 @@ import * as types from '../mutation-types'
 const state = {
   user: null,
   holidays: null,
+
+  //Global props, CAN change, not likely
+  leave_types: null,
+  performance_types: null,
+  employment_contract_types: null,
+
+  //Global props, more likely to change
+  contract_groups: null,
+  companies: null,
+
+  //User specific
+  timesheets: null,
+  contracts: null,
+
+  //Predefined
+  leave_statuses: ['PENDING', 'REJECTED', 'APPROVED', 'DRAFT'],
+  week_formatting: {
+    'workweek': {
+      'start': 1,
+      'end': 5
+    },
+    'weekend': {
+      'start': 6,
+      'end': 7
+    },
+    'fullweek': {
+      'start': 1,
+      'end': 7
+    }
+  }
 }
 
 // mutations
 const mutations = {
   [types.NINETOFIVER_SET_USER] (state, { user }) {
-    state.user = user
+    state.user = user;
+  },
+
+  [types.NINETOFIVER_SET_LEAVE_TYPES] (state, { leave_types }) {
+    state.leave_types = leave_types;
+  },
+  [types.NINETOFIVER_SET_PERFORMANCE_TYPES] (state, { performance_types }) {
+    state.performance_types = performance_types;
+  },
+  [types.NINETOFIVER_SET_EMPLOYMENT_CONTRACT_TYPES] (state, { employment_contract_types }) {
+    state.employment_contract_types = employment_contract_types;
+  },
+
+  [types.NINETOFIVER_SET_CONTRACT_GROUPS] (state, { contract_groups }) {
+    state.contract_groups = contract_groups;
+  },
+  [types.NINETOFIVER_SET_COMPANIES] (state, { companies }) {
+    state.companies = companies;
+  },
+
+  [types.NINETOFIVER_SET_CONTRACTS] (state, { contracts }) {
+    state.contracts = contracts;
+  },
+  [types.NINETOFIVER_SET_TIMESHEETS] (state, { timesheets }) {
+    state.timesheets = timesheets;
   }
 }
 
@@ -105,9 +159,173 @@ const actions = {
         reject(res)
       })
     })
-  }
-  
-}
+  },
+
+
+  [types.NINETOFIVER_RELOAD_LEAVE_TYPES] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/leave_types/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_LEAVE_TYPES, {
+          leave_types: res.data.results.map(x => {
+            return { id: x.id, name: x.type }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      });
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_PERFORMANCE_TYPES] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/performance_types/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_PERFORMANCE_TYPES, {
+          performance_types: res.data.results.map(x => {
+            return { id: x.id, name: x.label }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      });
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_EMPLOYMENT_CONTRACT_TYPES] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/employment_contract_types/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_EMPLOYMENT_CONTRACT_TYPES, {
+          employment_contract_types: res.data.results.map(x => {
+            return { id: x.id, name: x.label }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      });
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_CONTRACT_GROUPS] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/contract_groups/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_CONTRACT_GROUPS, {
+          contract_groups: res.data.results.map(x => {
+            return { id: x.id, name: x.label }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      })
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_COMPANIES] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/companies/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_COMPANIES, {
+          companies: res.data.results.map(x => {
+            return { id: x.id, name: x.name }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      })
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_CONTRACTS] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/my_contracts/'
+      }).then((res) => {
+
+        store.commit(types.NINETOFIVER_SET_CONTRACTS, {
+          contracts: res.data.results.map(x => {
+            return { 
+              id: x.id, 
+              name: x.label, 
+              customer: state.companies.find(x => x.id === x.customer).name
+            }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      })
+    });
+
+  },
+
+
+  [types.NINETOFIVER_RELOAD_TIMESHEETS] (store, options = {}) {
+
+    return new Promise((resolve, reject) => {
+      store.dispatch(types.NINETOFIVER_API_REQUEST, {
+        path: '/my_timesheets/'
+      }).then((res) => {
+
+        var today = new Date();
+        var timesheets = res.data.results.filter(x => {
+          return ( 
+            x.year < today.getYear() 
+            || (x.year == today.getYear() && x.month <= today.getMonth())
+          )
+        });
+
+        store.commit(types.NINETOFIVER_SET_TIMESHEETS, {
+          timesheets: timesheets.map(x => {
+              return { id: x.id, month: x.month, year: x.year, closed: x.closed }
+          })
+        });
+        resolve(res);
+
+      }, (res) => {
+        reject(res);
+      })
+    });
+
+  },
 
 export default {
   state,
