5aa75ae390349d18fcbb1d3960d54e6ff9c8693a
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-04-11 15:22:15 +0200

    Filter leaves based on day selected

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,15 +26,13 @@ div
       .card
         h4.card-title.text-md-center Absent collegues
         div.text-md-center
-          a(href='')
-            i.fa.fa-chevron-left 
-          | today
-          a(href='')
-            i.fa.fa-chevron-right
+          i.fa.fa-chevron-left(@click='dayEarlier')
+          | {{ selectedDay | moment('DD-MM') }}
+          i.fa.fa-chevron-right(@click='dayLater') 
         .cardblock
           table.table
             tbody
-              tr(v-for="(leave, index) in leaves" v-bind:key="leave.id")
+              tr(v-for="(leave, index) in leavesSelectedDay" v-bind:key="leave.id")
                 td {{ leave.display_label }}
         
   .row
@@ -52,7 +50,11 @@ import moment from 'moment';
 
 var data = {
   today: new Date(),
-  leaves: []
+  selectedDay: new Date(),
+  earliestLeave: new Date(),
+  latestLeave: new Date(),
+  leaves: [],
+  leavesSelectedDay: []
 }
 
 export default {
@@ -67,26 +69,31 @@ export default {
   },
 
   created: function () {
+    this.earliestLeave = moment();
+    this.latestLeave = moment();
     store.dispatch(types.NINETOFIVER_API_REQUEST, {
       path: '/leaves/',
     }).then((response) => {
-      this.today = moment()
+      this.selectedDay = moment()
       response.body.results.forEach((leave) => {
-          leave.leavedate_set.forEach(ld => {
-            ld.starts_at = moment(ld.starts_at, 'YYYY-MM-DD HH:mm:ss');
-            ld.ends_at = moment(ld.ends_at, 'YYYY-MM-DD HH:mm:ss');
-          });
-          if(leave.leavedate_set.length > 1){
-            //Check if today is between the start of the first leavedate and the last leavedate
-            if(this.today.isBetween(leave.leavedate_set[0].starts_at, leave.leavedate_set[leave.leavedate_set.length - 1].starts_at)){
-              this.leaves.push(leave);
-            }
-          } else {
-            if(leave.leavedate_set[0].starts_at.isSame(new Date(), 'day')){
-              this.leaves.push(leave);              
-            }
+        this.leaves.push(leave);
+        leave.leavedate_set.forEach(ld => {
+          ld.starts_at = moment(ld.starts_at, 'YYYY-MM-DD HH:mm:ss');
+          this.earliestLeave = ld.starts_at.isBefore(this.earliestLeave, 'day') ? ld.starts_at : this.earliestLeave;
+          ld.ends_at = moment(ld.ends_at, 'YYYY-MM-DD HH:mm:ss');
+          this.latestLeave = ld.ends_at.isAfter(this.latestLeave, 'day') ? ld.ends_at : this.latestLeave;          
+        });
+        if(leave.leavedate_set.length > 1){
+          //Check if today is between the start of the first leavedate and the last leavedate
+          if(this.selectedDay.isBetween(leave.leavedate_set[0].starts_at, leave.leavedate_set[leave.leavedate_set.length - 1].starts_at)){
+            this.leavesSelectedDay.push(leave);
           }
-      });
+        } else {
+          if(leave.leavedate_set[0].starts_at.isSame(this.selectedDay, 'day')){
+            this.leavesSelectedDay.push(leave);              
+          }
+        }
+      });         
     });
   },
 
@@ -112,7 +119,7 @@ export default {
     contracts: function() {
       if(store.getters.contracts)
         return store.getters.contracts;
-    },
+    }
 
   },
 
@@ -126,12 +133,49 @@ export default {
 
     roundHoursFilter: function(val) {
       return Math.round(val * 2) / 2;
-    },
-
+    }
   },
 
   methods: {
+    dayEarlier: function() {
+      this.leavesSelectedDay.splice(0);
+      this.selectedDay.subtract(1, 'days');      
+      // Check if selected day is between the start of the firs and last leave of the leaves
+      if(this.selectedDay.isBetween(this.earliestLeave, this.latestLeave)){
+        // Selected day is between leaves
+        this.leaves.filter((leave) => {
+          if(leave.leavedate_set.length > 1){
+            if(this.selectedDay.isBetween(leave.leavedate_set[0].starts_at, leave.leavedate_set[leave.leavedate_set.length - 1].starts_at)){
+              this.leavesSelectedDay.push(leave);
+            }
+          } else {
+            if(leave.leavedate_set[0].starts_at.isSame(this.selectedDay, 'day')){
+              this.leavesSelectedDay.push(leave);              
+            }
+          }
+        });
+      } else {
+        // Selected day is not between leaves
+        // Get next set of leaves
+      }
+    },
 
+    dayLater: function() {
+      this.leavesSelectedDay.splice(0);
+      this.selectedDay.add(1, 'days');      
+      // Check if selected day is between the start of the firs and last leave of the leaves
+      // Selected day is not between leaves
+      // Selected day is between leaves
+      this.leaves.filter((leave) => {
+        if(this.selectedDay.isBetween(leave.leavedate_set[0].starts_at, leave.leavedate_set[leave.leavedate_set.length - 1].starts_at)){
+          this.leavesSelectedDay.push(leave);
+        } else {
+          if(leave.leavedate_set[0].starts_at.isSame(this.selectedDay, 'day')){
+            this.leavesSelectedDay.push(leave);              
+          }
+        }
+      });
+    }
   },
 
   watch: { }
