b3219c5c2c994528bc35bc2c6afbb083966dca4f
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-04-19 16:24:25 +0200

    Groups have different colors now

--- src/components/Colleagues.vue
+++ src/components/Colleagues.vue
@@ -1,49 +1,52 @@
 <template lang="pug">
 div
-  .row
-    div.h3 Colleagues
+  .col-md-10
+    .row
+      h3 Colleagues
 
-  .row
-    .col-md-8
-      .btn-group(role='group' aria-label='Button group with nested dropdown')
-        button.btn.btn-secondary(type='button' @click='setSortBy("all")') All
-        .btn-group(role='group')
-          button.btn.btn-secondary.dropdown-toggle#btnGroupDrop(type='button' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false") {{ groupLabel }}
-          .dropdown-menu(aria-labelledby='btnGroupDrop')
-            //- button.btn.btn-secondary(v-for='group in groups' @click='setSortBy(group)') {{ group.name }}          
-            a.dropdown-item(v-for='group in groups' @click='setSortBy(group)') {{ group.name }}
-    .col-md-3
-      .input-group
-        span.input-group-addon Search
-        input(type='text', class='form-control', placeholder='Name, email, ...', v-model='query')
-      
+    .row
+      .col-md-8
+        .btn-group(role='group' aria-label='Button group with nested dropdown')
+          button.btn.btn-secondary(type='button' @click='setSortBy("all")') All
+          .btn-group(role='group')
+            button.btn.btn-secondary.dropdown-toggle#btnGroupDrop(type='button' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false") {{ groupLabel }}
+            .dropdown-menu(aria-labelledby='btnGroupDrop')
+              //- button.btn.btn-secondary(v-for='group in groups' @click='setSortBy(group)') {{ group.name }}          
+              a.dropdown-item(v-for='group in groups' @click='setSortBy(group)') {{ group.name }}
+      .col-md-3
+        .input-group
+          span.input-group-addon Search
+          input(type='text', class='form-control', placeholder='Name, email, ...', v-model='query')
+        
 
-  .row
-    .card-columns
-      #accordion(v-for='(user, index) in filteredUsers' role='tablist' aria-multiselectable='true') 
-        .card
-          .card-header( v-bind:id='"heading-" + index' role='tab' aria-expanded='false' v-bind:aria-controls='"collapse-" + index')
-            span.user-fullname {{ user.first_name }} {{ user.last_name }}
-            span(v-for='group in user.groups').tag.tag-primary.pull-right  {{ group | getGroupAsString }}
-          .card-block
-            .card-text
-              .row
-                .col-md-3 <strong>Email: </strong>
-                .col-md-9.text-md-right {{ user.email }}
-              .row
-                .col-md-3 <strong>Telephone: </strong>
-                .col-md-9.text-md-right +32 498 348585
-              .collapse(role='tabpanel' v-bind:id='"collapse-" + index' v-bind:aria-labelledby='"heading-" + index')
+    .row
+      .card-columns
+        #accordion(v-for='(user, index) in filteredUsers' role='tablist' aria-multiselectable='true') 
+          .card
+            .card-header( v-bind:id='"heading-" + index' role='tab' aria-expanded='false' v-bind:aria-controls='"collapse-" + index')
+              span.user-fullname {{ user.first_name }} {{ user.last_name }}
+              span(v-for='group in user.groups' v-bind:class='determineTagColor(group)').tag.pull-right  {{ group | getGroupAsString }}
+            .card-block
+              .card-text
                 .row
-                  .col-md-3 <strong>Birth Date: </strong>
-                  .col-md-9.text-md-right {{ user.birth_date | moment('DD MMMM YYYY') }}
+                  .col-md-3 <strong>Email: </strong>
+                  .col-md-9.text-md-right {{ user.email }}
                 .row
-                  .col-md-3 <strong>Gender: </strong>
-                  .col-md-9.text-md-right {{ user.gender | fullGender }}
-                .row
-                  .col-md-3 <strong>Country: </strong>
-                  .col-md-9.text-md-right {{ user.country }}    
-            button.btn.btn-sm.pull-right.toggle-info(data-toggle='collapse' v-bind:data-target='"#collapse-" + index' @click='hideOpen()') More info
+                  .col-md-3 <strong>Telephone: </strong>
+                  .col-md-9.text-md-right +32 498 348585
+                .collapse(role='tabpanel' v-bind:id='"collapse-" + index' v-bind:aria-labelledby='"heading-" + index')
+                  .row
+                    .col-md-3 <strong>Birth Date: </strong>
+                    .col-md-9.text-md-right {{ user.birth_date | moment('DD MMMM YYYY') }}
+                  .row
+                    .col-md-3 <strong>Gender: </strong>
+                    .col-md-9.text-md-right {{ user.gender | fullGender }}
+                  .row
+                    .col-md-3 <strong>Country: </strong>
+                    .col-md-9.text-md-right {{ user.country }}    
+              button.btn.btn-sm.pull-right.toggle-info(data-toggle='collapse' v-bind:data-target='"#collapse-" + index' @click='hideOpen()') More info
+  .col-md-2
+    h3 Advanced filter
 
 </template>
 
@@ -127,6 +130,17 @@ export default {
         this.sortBy = this.groups.find(x => x == value).id;
         this.groupLabel = this.groups.find(x => x == value).name;        
       }
+    },
+
+    determineTagColor: function(group_id) {
+      var group_name = store.getters.user_groups.find(group => group_id === group.id).name;
+      var tempObj = {
+        [store.getters.group_names[3]]: 'tag-primary',
+        [store.getters.group_names[2]]: 'tag-red',        
+        [store.getters.group_names[1]]: 'tag-blue',
+        [store.getters.group_names[0]]: 'tag-green',
+      }
+      return (tempObj[group_name]) ? tempObj[group_name] : 'tag-primary';   
     }
   },
 
@@ -153,6 +167,18 @@ export default {
 .tag {
   margin-left: 0.1rem;
   margin-right: 0.1rem;
+}
+
+.tag-red {
+  background-color: #ff0000;
+}
+
+.tag-blue {
+  background-color: #000080;
+}
+
+.tag-green {
+  background-color: #006400;
 }
 
 .user-fullname {
