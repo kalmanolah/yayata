838daefdebb13c31222fcbf32c9036859d2c89f9
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-05-17 11:06:42 +0200

    Admin page
    Displays leaveoverviewgrid

---
+++ src/components/Admin.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+div
+  .btn-group(role='group')
+    buttn.btn.btn-secondary(@click='currentView="LeaveOverviewGrid"') Leave Overview Grid
+    buttn.btn.btn-secondary(@click='currentView="Contracts"') Contracts
+  component(v-bind:is='currentView')
+</template>
+<script>
+import Vue from 'vue'
+import LeaveOverviewGrid from './LeaveOverviewGrid.vue'
+import Contracts from './Contracts.vue'
+
+export default {
+name: 'admin',
+components: {
+    LeaveOverviewGrid,
+    Contracts
+},
+data() {
+  return {
+    currentView: 'LeaveOverviewGrid'
+  }
+},
+methods: {
+
+ }
+}
+</script>
+<style>
+
+</style>
