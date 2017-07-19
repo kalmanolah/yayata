4170f3177ed5ff2b19beb88c55c29f4976dff38f
BartDeCaluwe <de.caluwe.bart@gmail.com>
2017-07-19 16:18:03 +0200

    query params added to time entries call

--- src/components/Redmine.vue
+++ src/components/Redmine.vue
@@ -248,7 +248,7 @@ export default {
               description: te.comments,
               performance_type: 1,
               contract: contract_id,
-              contract_role: 1,
+              contract_role: 2,
               redmine_id: te.id
             }
             // Create new performance
@@ -324,8 +324,7 @@ export default {
       // Reload redmine time entries for this user and this month
       store.dispatch(types.NINETOFIVER_RELOAD_REDMINE_TIME_ENTRIES, {
         params: {
-          user_id: store.getters.user.redmine_id,
-          month: moment().month() + 1
+          filter_imported: false
         }
       });
     },
