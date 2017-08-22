a3fff141aca236f88e035866212530d88f7aded0
Bram <bramvereertbrugghen@live.be>
2017-08-22 14:51:57 +0200

    Sidebar is now responsive, gets replaced by a hamburgermenu with a modal on small devices

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,34 @@
 <template lang="pug">
 .row
-  .col.navbar-yayata.d-flex.justify-content-end
-    nav(
-      class='navbar navbar-expand'
-    ) 
+  .col.navbar-yayata.d-flex.mr-auto
+
+    .col.pull-left.d-md-none
+      b-btn.m-2(v-b-modal="'modalio'")
+        i.fa.fa-bars
+      b-modal#modalio(
+        ref='modalio'
+        hide-header=true
+        hide-footer=true
+        ok-disabled=true
+      )
+        nav.text-center(@click='hideModal()')
+          router-link(:to='{ name: "timesheets" }')
+            h2 Timesheets 
+          router-link(:to='{ name: "contracts" }')
+            h2 Contracts
+          router-link(:to='{ name: "leaves" }')
+            h2 Leaves
+          router-link(:to='{ name: "colleagues", params: { userId: "all"}}')
+            h2 Colleagues
+          router-link(:to='{ name: "calendar_month_redirect" }')
+            h2 Calendar
+          router-link(:to='{ name: "redmine" }')
+            h2 Redmine
+
+    .col.navbar.navbar-expand
       .collapse.navbar-collapse#navbarDropdown
-        ul(
-          class='navbar-nav'
-        ) 
-          li(
-            class='nav-item dropdown'
-          )
+        ul.navbar-nav.ml-auto
+          li.nav-item.dropdown
             a(
               class='nav-link dropdown-toggle'
               v-if='user'
@@ -20,8 +38,7 @@
               aria-haspopup='true'
               aria-expanded='false'
             ) {{ user.display_label }}
-            div(
-              class='dropdown-menu'
+            .dropdown-menu(
               aria-labelledby='responsiveNavbarDropdown'
             )
               router-link.dropdown-item#admin-nav(:to='{ name: "admin" }') Admin
@@ -44,7 +61,11 @@ export default {
         return store.getters.user;
     }
   },
-
+  methods: {
+    hideModal() {
+        this.$refs.modalio.hide();
+    }
+  },
   data () {
     return {
       clicked: false
